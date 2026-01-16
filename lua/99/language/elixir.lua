f0e43c189f38586f555233b66b40c4d20105bd50
Rajiv <millord237@gmail.com>
2026-01-16 16:54:46 -0500

    Fix elixir lua files format issues

--- lua/99/language/elixir.lua
+++ lua/99/language/elixir.lua
@@ -5,7 +5,7 @@ M.names = {}
 --- @param item_name string
 --- @return string
 function M.log_item(item_name)
-    return string.format('IO.inspect(%s, label: "%s")', item_name, item_name)
+  return string.format('IO.inspect(%s, label: "%s")', item_name, item_name)
 end
 
 return M
